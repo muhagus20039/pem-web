ce2e00b28a6a6afe74492912a3ce7994c03a69ba
muhagus20039 <muhagussaputra20039@gmail.com>
2022-01-10 14:23:24 +0800

    Tampilan Billboard

--- index.php
+++ index.php
@@ -1,14 +1,171 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>
-        SELAMAT
-    </h1>
-</body>
+  <head>
+    <script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Tangga Lagu Indonesia</title>
+  </head>
+  <body>
+    <div class="container fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mt-2">
+    <div class="container-fluid">
+        <span>
+            <img src="img/logo.png" alt="" width="45" height="30">
+        </span>
+        <a class="navbar-brand" href="#">Tangga Lagu Indonesia</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link" href="#">Billboard</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Lirik</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Dengarkan</a>
+            </li>
+        </ul>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        </div>
+    </div>
+    </nav>
+
+    <figure class="text-center mt-4">
+        <blockquote class="blockquote">
+            <p>Top 10 Billboard This Week</p>
+        </blockquote>
+    </figure>
+
+    <table class="table table-hover table-bordered align-middle">
+        <thead>
+            <tr>
+            <th scope="col">No</th>
+            <th scope="col">Lagu</th>
+            <th scope="col">Artis</th>
+            <th scope="col">Plays</th>
+            <th scope="col">Dengarkan</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>abcdefu</td>
+            <td>GAYLE</td>
+            <td>4.795.710</td>
+            <td>
+                <a href="https://youtu.be/NaFd8ucHLuo"><span class="iconify" data-icon="ant-design:play-circle-filled"></span></a>
+            </td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Sacrifice</td>
+            <td>The Weeknd</td>
+            <td>4.760.913</td>
+            <td>
+                <a href="https://youtu.be/VafTMsrnSTU"><span class="iconify" data-icon="ant-design:play-circle-filled"></span></a>
+            </td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td>STAY</td>
+            <td>The Kid Laroi, Justin Bieber</td>
+            <td>4.554.901</td>
+            <td>
+                <a href="https://youtu.be/kTJczUoc26U"><span class="iconify" data-icon="ant-design:play-circle-filled"></span></a>
+            </td>
+            </tr>
+            <tr>
+            <th scope="row">4</th>
+            <td>Heat Waves</td>
+            <td>Glass Animals</td>
+            <td>4.266.226</td>
+            <td>
+                <a href="https://youtu.be/mRD0-GxqHVo"><span class="iconify" data-icon="ant-design:play-circle-filled"></span></a>
+            </td>
+            </tr>
+            <tr>
+            <th scope="row">5</th>
+            <td>Easy on Me</td>
+            <td>Adele</td>
+            <td>4.012.162</td>
+            <td>
+                <a href="https://youtu.be/X-yIEMduRXk"><span class="iconify" data-icon="ant-design:play-circle-filled"></span></a>
+            </td>
+            </tr>
+            <tr>
+            <th scope="row">6</th>
+            <td>Title</td>
+            <td>Meghan Trainor</td>
+            <td>4.000.162</td>
+            <td>
+                <a href="https://youtu.be/fD7LIqkKisc"><span class="iconify" data-icon="ant-design:play-circle-filled"></span></a>
+            </td>
+            </tr>
+            <tr>
+            <th scope="row">7</th>
+            <td>Minefields</td>
+            <td>Faouzia & John Legends</td>
+            <td>3.812.162</td>
+            <td>
+                <a href="https://youtu.be/jKIEUdAMtrQ"><span class="iconify" data-icon="ant-design:play-circle-filled"></span></a>
+            </td>
+            </tr>
+            <tr>
+            <th scope="row">8</th>
+            <td>Bad Habbits</td>
+            <td>Ed Sheeran</td>
+            <td>3.732.162</td>
+            <td>
+                <a href="https://youtu.be/orJSJGHjBLI"><span class="iconify" data-icon="ant-design:play-circle-filled"></span></a>
+            </td>
+            </tr>
+            <tr>
+            <th scope="row">9</th>
+            <td>MONTERO (Call Me by Your Name)</td>
+            <td>Lil Nas X</td>
+            <td>3.672.162</td>
+            <td>
+                <a href="https://youtu.be/6swmTBVI83k"><span class="iconify" data-icon="ant-design:play-circle-filled"></span></a>
+            </td>
+            </tr>
+            <tr>
+            <th scope="row">10</th>
+            <td>I Love You So</td>
+            <td>The Walters</td>
+            <td>3.412.162</td>
+            <td>
+                <a href="https://youtu.be/WBRPnyMT164"><span class="iconify" data-icon="ant-design:play-circle-filled"></span></a>
+            </td>
+            </tr>
+        </tbody>
+    </table>
+
+
+    </div>
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
